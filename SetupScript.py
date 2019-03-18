5f3f64d968a3586e3a0cc981057d864aed4db5c1
connorcalvey <42801998+connorcalvey@users.noreply.github.com>
2019-03-18 17:30:06 +0000

    Update SetupScript.py

--- SetupScript.py
+++ SetupScript.py
@@ -127,8 +127,8 @@ os.system('brew cleanup && brew cask cleanup')
 
 # Installing Printer and driver
 print "Installing INKBOT Printer"
-os.system('sudo curl -o /Library/Printers/PPDs.zip https://s3.eu-west-2.amazonaws.com/tabinkbotprinterdeploytest/tabinkbotdeploytest/PPDs.zip')
-os.system('sudo curl -o /Library/Printers/Xerox.zip https://s3.eu-west-2.amazonaws.com/tabinkbotprinterdeploytest/tabinkbotdeploytest/Xerox.zip')
+os.system('sudo curl -o /Library/Printers/PPDs.zip https://s3-eu-west-1.amazonaws.com/it-services/XeroxC8055Script/PPDs.zip')
+os.system('sudo curl -o /Library/Printers/Xerox.zip https://s3-eu-west-1.amazonaws.com/it-services/XeroxC8055Script/Xerox.zip')
 os.system('sudo unzip /Library/Printers/PPDs.zip')
 os.system('sudo unzip /Library/Printers/Xerox.zip -d /Library/Printers/')
 os.system('lpadmin -p INKBOT -L "2nd Floor, The Spitfire Building" -E -v ipp://10.2.0.24 -o printer-is-shared=“False”  -P /Library/Printers/PPDs/Contents/Resources/Xerox\ AltaLink\ C8055.gz')
