0b3e74cf5c680352640da5a56a64c9e8990744af
connorcalvey <42801998+connorcalvey@users.noreply.github.com>
2019-05-09 16:20:53 +0100

    Update SetupScript.py

--- SetupScript.py
+++ SetupScript.py
@@ -101,11 +101,6 @@ os.system('brew cask install qlcolorcode qlmarkdown quicklook-csv quicklook-json
 print "Installing Essential Apps"
 os.system('brew cask install spotify slack zoomus google-backup-and-sync google-chrome')
 
-# Installing Printer and drive
-print "Installing TAB Printer"
-os.system('sudo curl -o /Library/Printers/PPDs/Contents/Resources/Xerox\ AltaLink\ C8055.gz https://s3-eu-west-1.amazonaws.com/it-services/Backgrounds/Xerox+AltaLink+C8055.gz')
-os.system('lpadmin -p INKBOT -L "2nd Floor, The Spitfire Building" -E -v ipp://10.2.0.24 -o printer-is-shared=“False”  -P /Library/Printers/PPDs/Contents/Resources/Xerox\ AltaLink\ C8055.gz')
-
 #Random OSX Settings
 print "Tweaking OSX Settings"
 
@@ -131,6 +126,7 @@ os.system('sudo curl -o /Library/Printers/PPDs.zip https://s3-eu-west-1.amazonaw
 os.system('sudo curl -o /Library/Printers/Xerox.zip https://s3-eu-west-1.amazonaws.com/it-services/XeroxC8055Script/Xerox.zip')
 os.system('sudo unzip /Library/Printers/PPDs.zip')
 os.system('sudo unzip /Library/Printers/Xerox.zip -d /Library/Printers/')
+os.system('sudo curl -o /Library/Printers/PPDs/Contents/Resources/Xerox\ AltaLink\ C8055.gz https://s3-eu-west-1.amazonaws.com/it-services/Backgrounds/Xerox+AltaLink+C8055.gz')
 os.system('lpadmin -p INKBOT -L "2nd Floor, The Spitfire Building" -E -v ipp://10.2.0.24 -o printer-is-shared=“False”  -P /Library/Printers/PPDs/Contents/Resources/Xerox\ AltaLink\ C8055.gz')
 
 show_notification("Script Complete :)")
