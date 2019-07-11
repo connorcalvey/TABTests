63613e44d4cfb4950716cc0c1c15041d4ce82083
connorcalvey <42801998+connorcalvey@users.noreply.github.com>
2019-07-11 11:28:25 +0100

    Create Xerox8055SetupScript.py

---
+++ Xerox8055SetupScript.py
@@ -0,0 +1,8 @@
+# Installing Printer and driver
+print "Installing INKBOT Printer"
+os.system('sudo curl -o /Library/Printers/PPDs.zip https://s3-eu-west-1.amazonaws.com/it-services/XeroxC8055Script/PPDs.zip')
+os.system('sudo curl -o /Library/Printers/Xerox.zip https://s3-eu-west-1.amazonaws.com/it-services/XeroxC8055Script/Xerox.zip')
+os.system('sudo unzip /Library/Printers/PPDs.zip')
+os.system('sudo unzip /Library/Printers/Xerox.zip -d /Library/Printers/')
+os.system('sudo curl -o /Library/Printers/PPDs/Contents/Resources/Xerox\ AltaLink\ C8055.gz https://s3-eu-west-1.amazonaws.com/it-services/Backgrounds/Xerox+AltaLink+C8055.gz')
+os.system('lpadmin -p OfficePrinter -L "2nd Floor, The Spitfire Building" -E -v ipp://10.5.0.10 -o printer-is-shared=“False”  -P /Library/Printers/PPDs/Contents/Resources/Xerox\ AltaLink\ C8055.gz')
