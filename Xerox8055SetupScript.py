172ff3f089db8dc572a56cedb4933daa9f794cce
connorcalvey <42801998+connorcalvey@users.noreply.github.com>
2019-07-18 16:09:01 +0100

    Update Xerox8055SetupScript.py

--- Xerox8055SetupScript.py
+++ Xerox8055SetupScript.py
@@ -19,10 +19,10 @@ if os.system('xcode-select -p') != 0:
 
 # Installing Printer and driver
 
-print "Installing Office Printer"
+print "Installing TAB-INKBOT"
 os.system('sudo curl -o /Library/Printers/PPDs.zip https://s3-eu-west-1.amazonaws.com/it-services/XeroxC8055Script/PPDs.zip')
 os.system('sudo curl -o /Library/Printers/Xerox.zip https://s3-eu-west-1.amazonaws.com/it-services/XeroxC8055Script/Xerox.zip')
 os.system('sudo unzip /Library/Printers/PPDs.zip')
 os.system('sudo unzip /Library/Printers/Xerox.zip -d /Library/Printers/')
 os.system('sudo curl -o /Library/Printers/PPDs/Contents/Resources/Xerox\ AltaLink\ C8055.gz https://s3-eu-west-1.amazonaws.com/it-services/Backgrounds/Xerox+AltaLink+C8055.gz')
-os.system('lpadmin -p OfficePrinter -L "2nd Floor, The Spitfire Building" -E -v ipp://10.5.0.10 -o printer-is-shared=“False”  -P /Library/Printers/PPDs/Contents/Resources/Xerox\ AltaLink\ C8055.gz')
+os.system('lpadmin -p TAB-INKBOT -L "2nd Floor, The Spitfire Building" -E -v ipp://10.5.0.10 -o printer-is-shared=“False”  -P /Library/Printers/PPDs/Contents/Resources/Xerox\ AltaLink\ C8055.gz')
